2b656d25202e1280d0a8f54642b0e02e45cddff6
Dede1421 <mauricioandre91@gmail.com>
2024-12-05 18:40:05 -0500

    feat: add a routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { FormsComponent } from './components/forms/forms.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full',
+  },
+  {
+    path: 'forms',
+    component: FormsComponent,
+  },
+];
