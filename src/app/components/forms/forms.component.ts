07c88991a942eda50e209a132bd3cb4a2d9bc534
Dede1421 <mauricioandre91@gmail.com>
2024-12-05 16:28:01 -0500

    feat: create a form component

---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  imports: [],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.scss',
+})
+export class FormsComponent {}
