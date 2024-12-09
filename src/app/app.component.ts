76a1a7905783207304abb7a6d2dc1024907c381c
Dede1421 <mauricioandre91@gmail.com>
2024-12-09 18:19:59 -0500

    style: add a material inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsComponent } from "./components/forms/forms.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Ophtalmic-center';
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss',
 })
