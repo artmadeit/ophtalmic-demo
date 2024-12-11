549970175128ae2dbbda691fe768b80b3195816b
Dede1421 <mauricioandre91@gmail.com>
2024-12-11 10:24:03 -0500

    style: change the order of inputs in Affiliation form

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,23 +1,36 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-forms',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, ReactiveFormsModule],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+    MatGridListModule,
+  ],
   providers: [provideNativeDateAdapter()],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss',
 })
 export class FormsComponent {
-  personForm: FormGroup
+  personForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.personForm = fb.group({
-      phone: ['']
-    })
+      phone: [''],
+    });
   }
 }
