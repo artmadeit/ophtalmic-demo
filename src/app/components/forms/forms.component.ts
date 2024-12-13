b82413ddf19fec827d1ec0db2133a3e66171aed8
Dede1421 <mauricioandre91@gmail.com>
2024-12-13 09:49:08 -0500

    feat: add new option in document type select

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -37,18 +37,22 @@ export class FormsComponent {
   identifyDocuments: IdentifyDocument[] = [
     { value: 'id', viewValue: 'DNI' },
     { value: 'passport', viewValue: 'PASAPORTE' },
+    { value: 'foreigner card', viewValue: 'CARNET DE EXTRANJERIA' },
   ];
 
   constructor(private fb: FormBuilder) {
     this.personForm = fb.group({
       name: '',
       lastName: '',
-      age: '',
       job: '',
       phone: [''],
       idCard: '',
       email: '',
       address: '',
     });
+  }
+
+  get age() {
+    return 31;
   }
 }
