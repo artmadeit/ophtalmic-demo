c2fbf980c5fd6b27c7b97cd059f95d516c199a95
artmadeit <this.arthur.email@gmail.com>
2025-01-05 12:32:22 -0500

    feat: add auth intercepteor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,16 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 import { provideAuth0 } from '@auth0/auth0-angular';
+import { authInterceptor } from './core/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), 
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     {
       provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
       useValue: {
