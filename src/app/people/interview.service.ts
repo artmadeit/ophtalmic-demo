c20c308a261f4cd041329c49bda9d28347e94d27
Dede1421 <mauricioandre91@gmail.com>
2025-02-04 16:20:38 -0500

    feat: add findAll and findById

--- src/app/people/interview.service.ts
+++ src/app/people/interview.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { InterviewForm } from './InterviewForm';
@@ -10,6 +10,16 @@ export class InterviewService {
   private baseUrl = `${environment.apiUrl}/interviews`;
 
   constructor(private http: HttpClient) {}
+
+  findById(id: number) {
+    return this.http.get<InterviewForm>(`${this.baseUrl}/${id}`)
+  }
+
+  findAll(personId: number) {
+    let params = new HttpParams().set('personId', personId);
+
+    return this.http.get<InterviewForm[]>(`${this.baseUrl}`, { params });
+  }
 
   register(interview: InterviewForm) {
     return this.http.post<InterviewForm>(this.baseUrl, interview);
